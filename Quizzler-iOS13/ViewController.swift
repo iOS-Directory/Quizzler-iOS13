521853fe1a0aaa84f355715fd6e27d09be69abda
fritzgt <tuanis@live.com>
2019-11-24 11:33:53 +0200

    created question and functionality to iterate over the array when button is clicked

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //Liked all the components
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    let quiz = [
+    "Four + Two is equal to Six",
+    "Five - Three is greater than One",
+    "Three + Eight is less than Ten"
+    ]
+    //Start with first question
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUi()
     }
 
+    //Here I Linked the true button and changed the
+    //type to uiButton and then drag the dot to the
+    //false button as well
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        if(questionNumber < quiz.count - 1){
+            print(questionNumber)
+            questionNumber += 1
+            updateUi()
+        }else{
+            questionLabel.text = "Completed!"
+        }
+ 
+//        if(sender.currentTitle! == "True"){
+//            print("Correct")
+//        }else{
+//            print("Incorrect!")
+//        }
+    }
+    
+    //Create function to avoid repeating code
+    func updateUi(){
+        questionLabel.text = quiz[questionNumber]
+    }
 
 }
 
