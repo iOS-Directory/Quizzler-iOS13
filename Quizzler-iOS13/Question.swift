34dda2f34b073baf2e6103742e9a516f5e79e858
fritzgt <tuanis@live.com>
2019-11-24 21:34:54 +0200

    added new questions to the struct

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,22 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by FGT MAC on 11/24/19.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct Question {
+    let question: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        question = q
+        answer = a
+    }
+    
+}
+ 
